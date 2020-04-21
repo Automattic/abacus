60acad5a39975400aaff95f3829fe57f60206606
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-04-21 16:48:32 -0600

    Add Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,7 @@ const IndexPage = () => (
   <Layout title="Experiments">
     <img src="/img/logo.png" width="100" />
     <h1>Experiments</h1>
-    <p>
-      Table of experiments to go here.
-    </p>
+    <p>Table of experiments to go here.</p>
   </Layout>
 )
 
