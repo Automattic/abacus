e3c99a675cfc0e842aafd7a531dee2149bc8f087
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-04-27 18:09:34 -0600

    Fix Format

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
-  addons: [
-    '@storybook/preset-scss',
-    '@storybook/preset-typescript',
-  ],
-  stories: [
-    '../**/*.stories.tsx',
-  ],
+  addons: ['@storybook/preset-scss', '@storybook/preset-typescript'],
+  stories: ['../**/*.stories.tsx'],
 }
