dd13d24935d5555008895be111a8ad8cf8475da7
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-05-07 20:14:32 -0600

    Clarify Comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -61,7 +61,8 @@ module.exports = {
     ],
     'no-multiple-empty-lines': ['error', { max: 2, maxBOF: 1, maxEOF: 1 }],
 
-    // Disabling the base rule as it can report incorrect errors.
+    // Disabling the base rule as it can report incorrect errors and is recommended by
+    // @typescript-eslint/no-unused-vars.
     'no-unused-vars': 'off',
 
     'padded-blocks': 'off',
