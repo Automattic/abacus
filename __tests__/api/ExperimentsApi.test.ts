a29c374545252e3e0aeb62c69ef87f3ace86f37c
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-05-28 11:50:47 -0600

    Add Test for ExperimentsApi.create

--- __tests__/api/ExperimentsApi.test.ts
+++ __tests__/api/ExperimentsApi.test.ts
@@ -1,11 +1,40 @@
 import ExperimentsApi from '@/api/ExperimentsApi'
 
-import { Platform, Status } from '@/models/index'
+import { ExperimentFull, Platform, Status } from '@/models/index'
 
 const PLATFORMS = Object.values(Platform)
 const STATUSES = Object.values(Status)
 
 describe('ExperimentsApi.ts module', () => {
+  describe('create', () => {
+    it('should create a new experiment', async () => {
+      const apiData = await ExperimentsApi.create(
+        new ExperimentFull({
+          experimentId: null,
+          name: 'my_experiment',
+          startDatetime: new Date(Date.UTC(2020, 4, 1)),
+          endDatetime: new Date(Date.UTC(2020, 4, 4)),
+          status: Status.Staging,
+          platform: Platform.Wpcom,
+          ownerLogin: 'wp_johnsmith',
+          description: 'My first experiment.',
+          existingUsersAllowed: true,
+          p2Url: 'https://betterexperiments.a8c.com/2020-04-28/my-experiment',
+          variations: [],
+          segmentAssignments: [],
+          metricAssignments: [],
+        }),
+      )
+      // We expect that the response will return the new experiment with its newly
+      // assigned ID. These integration tests test against the "development" API which
+      // only returns mock data. So, instead of trying to keep in sync with the actual
+      // mock values, we'll test that it is valid to create an instance from the data.
+      expect(apiData).toBeDefined()
+      expect(() => {
+        ExperimentFull.fromApiData(apiData)
+      }).not.toThrowError()
+    })
+  })
   describe('findAll', () => {
     it('should return a set of experiments with the expected experiment shape', async () => {
       const experiments = await ExperimentsApi.findAll()
