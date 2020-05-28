45945f2245a1e670d53f9850d4bafc200dfc328d
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-05-27 22:17:47 -0600

    Add Documentation

--- models/ExperimentBare.ts
+++ models/ExperimentBare.ts
@@ -4,7 +4,15 @@ import { ApiData } from '@/api/ApiData'
 
 import { Platform, Status } from './index'
 
+/**
+ * An experiment with select data.
+ */
 export class ExperimentBare {
+  /**
+   * Create an instance from raw API data (parsed JSON).
+   *
+   * @param apiData Raw API data.
+   */
   static fromApiData(apiData: ApiData) {
     return {
       experimentId: apiData.experiment_id,
@@ -17,6 +25,19 @@ export class ExperimentBare {
     }
   }
 
+  /**
+   * @param experimentId - Unique experiment ID.
+   * @param name - Name of the experiment.
+   * @param startDatetime - Start date of the experiment. For new experiments, the
+   *   date must be in the future to accommodate forward planning of experiments.
+   * @param endDatetime - End date of the experiment. This value must be greater than
+   *   `start_datetime`. The server may impose a limited difference between\
+   *   `end_datetime` and `start_datetime` to ensure that experiments don't run for
+   *   too long.
+   * @param status -
+   * @param platform -
+   * @param ownerLogin - The login name of the experiment owner.
+   */
   constructor(
     public readonly experimentId: number | null,
     public readonly name: string,
