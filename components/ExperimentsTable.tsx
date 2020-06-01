f71f6dd77ddacba62e3147acfd1407e0db4d7564
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-01 17:26:47 -0600

    Add Column Sorting Done w/o external library dependency.

--- components/ExperimentsTable.tsx
+++ components/ExperimentsTable.tsx
@@ -16,6 +16,27 @@ interface Props {
   experiments: ExperimentBare[]
 }
 
+const createSortByComparable = (
+  name: 'endDatetime' | 'name' | 'ownerLogin' | 'platform' | 'startDatetime' | 'status',
+) => {
+  return (a: ExperimentBare, b: ExperimentBare) => {
+    if (a[name] < b[name]) {
+      return -1
+    }
+    if (a[name] > b[name]) {
+      return 1
+    }
+    return 0
+  }
+}
+
+const sortByEndDatetime = createSortByComparable('endDatetime')
+const sortByName = createSortByComparable('name')
+const sortByOwnerLogin = createSortByComparable('ownerLogin')
+const sortByPlatform = createSortByComparable('platform')
+const sortByStartDatetime = createSortByComparable('startDatetime')
+const sortByStatus = createSortByComparable('status')
+
 /**
  * Renders a table of "bare" experiment information.
  */
@@ -28,33 +49,39 @@ const ExperimentsTable = (props: Props) => {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
+      sorter: sortByName,
     },
     {
       title: 'Start',
       dataIndex: 'startDatetime',
       key: 'startDatetime',
       render: isoDateRenderer,
+      sorter: sortByStartDatetime,
     },
     {
       title: 'End',
       dataIndex: 'endDatetime',
       key: 'endDatetime',
       render: isoDateRenderer,
+      sorter: sortByEndDatetime,
     },
     {
       title: 'Status',
       dataIndex: 'status',
       key: 'status',
+      sorter: sortByStatus,
     },
     {
       title: 'Platform',
       dataIndex: 'platform',
       key: 'platform',
+      sorter: sortByPlatform,
     },
     {
       title: 'Owner',
       dataIndex: 'ownerLogin',
       key: 'ownerLogin',
+      sorter: sortByOwnerLogin,
     },
   ]
 
