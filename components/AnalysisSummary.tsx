297a518b61330b505e9a1d96d51177f100e7bbde
Yanir Seroussi <yanir.seroussi@automattic.com>
2020-06-04 17:17:48 +1000

    Add count table

--- components/AnalysisSummary.tsx
+++ components/AnalysisSummary.tsx
@@ -1,40 +1,80 @@
-import {Analysis, ExperimentFull} from "@/models";
-import _ from "lodash";
-import {formatIsoUtcOffset} from "@/utils/date";
-import React from "react";
+import { Analysis, AnalysisStrategy, ExperimentFull } from '@/models'
+import _ from 'lodash'
+import { formatIsoUtcOffset } from '@/utils/date'
+import React from 'react'
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core'
 
-export default function AnalysisSummary(props: { analyses: Analysis[], experiment: ExperimentFull }) {
+// TODO: move?
+const strategyToTitle = {
+  [AnalysisStrategy.IttPure]: 'All participants',
+  [AnalysisStrategy.MittNoCrossovers]: 'Without crossovers',
+  [AnalysisStrategy.MittNoSpammers]: 'Without spammers',
+  [AnalysisStrategy.MittNoSpammersNoCrossovers]: 'Without crossovers and spammers',
+  [AnalysisStrategy.PpNaive]: 'Exposed without crossovers and spammers',
+}
+
+export default function AnalysisSummary(props: { analyses: Analysis[]; experiment: ExperimentFull }) {
   const { analyses, experiment } = props
   if (analyses.length === 0) {
     return <h2>No analyses yet.</h2>
   }
-  const sortedAnalyses = _.reverse(_.sortBy(analyses, ['analysisDatetime']))
+  const sortedAnalyses = _.orderBy(analyses, ['analysisDatetime'], ['desc'])
   const latestAnalysisDatetime = sortedAnalyses[0].analysisDatetime
-  const latestAnalyses = _.filter(sortedAnalyses, ['analysisDatetime', latestAnalysisDatetime])
-  const metricAssignmentIds = _.map(experiment.metricAssignments, 'metricAssignmentId')
+  // TODO: is order still guaranteed?
+  const metricAssignmentIdToSortedAnalyses = _.groupBy(sortedAnalyses, 'metricAssignmentId')
+  const primaryMetricAssignmentId = _.filter(experiment.metricAssignments, ['isPrimary', true])[0]
+    .metricAssignmentId as number
+  const latestPrimaryMetricAnalyses = _.sortBy(
+    _.filter(metricAssignmentIdToSortedAnalyses[primaryMetricAssignmentId], [
+      'analysisDatetime',
+      metricAssignmentIdToSortedAnalyses[primaryMetricAssignmentId][0].analysisDatetime,
+    ]),
+    'analysisStrategy',
+  )
+  const sortedVariations = _.orderBy(experiment.variations, ['isPrimary', 'name'], ['desc', 'asc'])
   // TODO:
-  // - add participant counts according to the primary metric
-  // - complain if a metric assignment doesn't have data for the latest analysis date
-  // - use a nice table (Material once merged)
+  // - complain/warn if a metric assignment doesn't have data for the latest analysis date -- might actually be fine because of the attribution window
   // - add metric assignment values
-  // - probably extract component and test
+  // - handle edge cases
+  // - add some light tests
+  // - move not final to metric assignment summary -- confusing under counts
   return (
     <>
       <h2>Analysis summary for {formatIsoUtcOffset(latestAnalysisDatetime)}</h2>
-      <p>{latestAnalyses.length} analyses found ({analyses.length} including historical).</p>
-      <table>
-        <thead>
-        <tr>
-          <th>Strategy</th>
-          <th>Total</th>
-          <th>Not final</th>
-          <th>Variation split</th>
-        </tr>
-        </thead>
-        <tbody>
-        </tbody>
-      </table>
-      <pre>{JSON.stringify(analyses, null, 2)}</pre>
+      <p>Found {analyses.length} analysis objects in total.</p>
+
+      <h3>Participant counts</h3>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Strategy</TableCell>
+              <TableCell>Total</TableCell>
+              <TableCell>Not final</TableCell>
+              <TableCell>Variation split</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {latestPrimaryMetricAnalyses.map((analysis: Analysis) => (
+              <TableRow key={analysis.analysisStrategy}>
+                <TableCell>{strategyToTitle[analysis.analysisStrategy]}</TableCell>
+                <TableCell>{analysis.participantStats.total}</TableCell>
+                <TableCell>{analysis.participantStats.not_final}</TableCell>
+                <TableCell>
+                  {sortedVariations.map((variation) => (
+                    <div key={variation.name}>
+                      <code>{variation.name}</code>:&nbsp;
+                      <span>{analysis.participantStats[`variation_${variation.variationId}`] || 0}</span>
+                    </div>
+                  ))}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      {/*<pre>{JSON.stringify(analyses, null, 2)}</pre>*/}
     </>
   )
 }
