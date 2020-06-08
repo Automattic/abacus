27a562872ce44f7f4aeed8676abd2be2f4209759
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-08 13:00:37 -0600

    Order to Match Enum Order

--- components/AttributionWindow.tsx
+++ components/AttributionWindow.tsx
@@ -3,25 +3,21 @@ import React from 'react'
 import { AttributionWindowSeconds } from '@/models'
 
 const TRANSLATION_TABLE = {
-  [AttributionWindowSeconds.FourWeeks]: '4 weeks',
   [AttributionWindowSeconds.OneHour]: '1 hour',
-  [AttributionWindowSeconds.OneWeek]: '1 week',
   [AttributionWindowSeconds.SixHours]: '6 hours',
-  [AttributionWindowSeconds.ThreeDays]: '3 days',
-  [AttributionWindowSeconds.ThreeWeeks]: '3 weeks',
   [AttributionWindowSeconds.TwelveHours]: '12 hours',
   [AttributionWindowSeconds.TwentyFourHours]: '24 hours',
+  [AttributionWindowSeconds.ThreeDays]: '3 days',
+  [AttributionWindowSeconds.OneWeek]: '1 week',
   [AttributionWindowSeconds.TwoWeeks]: '2 weeks',
-}
-
-interface Props {
-  attributionWindowSeconds: AttributionWindowSeconds
+  [AttributionWindowSeconds.ThreeWeeks]: '3 weeks',
+  [AttributionWindowSeconds.FourWeeks]: '4 weeks',
 }
 
 /**
  * Renders the attribution window in concise, human readable text.
  */
-const AttributionWindow = (props: Props) => {
+const AttributionWindow = (props: { attributionWindowSeconds: AttributionWindowSeconds }) => {
   return <span>{TRANSLATION_TABLE[props.attributionWindowSeconds]}</span>
 }
 
