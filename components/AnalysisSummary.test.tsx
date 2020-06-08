57878deab386fd9fcb3884c7fb0eb74da2994350
Yanir Seroussi <yanir.seroussi@automattic.com>
2020-06-08 17:17:10 +1000

    Remove comment

--- components/AnalysisSummary.test.tsx
+++ components/AnalysisSummary.test.tsx
@@ -182,18 +182,3 @@ test('shows the analyses JSON in debug mode', () => {
   )
   expect(container.querySelector('pre')).not.toBeNull()
 })
-
-// if (analyses.length === 0) {
-//   return <h2>No analyses yet for {experiment.name}.</h2>
-// }
-//
-// return (
-//   <>
-//     <h2>Analysis summary</h2>
-//     <p>
-//       Found {analyses.length} analysis objects in total. There are {metrics.length} metrics in the system.
-//     </p>
-//
-//     {debugMode ? <pre>{JSON.stringify(analyses, null, 2)}</pre> : ''}
-//   </>
-// )
