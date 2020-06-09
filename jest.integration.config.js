6b5940572b5db1a9711c901b4ea88b2fc973e26f
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-09 15:24:05 -0600

    Add Coverage Reporting for Integration Testing

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,4 +1,19 @@
 module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/api/**/*.ts',
+    '!**/node_modules/**',
+    // FIXME: TODO: Get tests on the following.
+    '!<rootDir>/api/UnauthorizedError.ts',
+    '!<rootDir>/api/utils.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   globals: {
     // Must specify a custom tsconfig for tests because we need the TypeScript
     // transform to transform JSX into js rather than leaving it as JSX which the
