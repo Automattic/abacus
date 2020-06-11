738e08e4b622522c28198283e2d9bb50da2292e2
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-11 11:18:16 -0600

    Remove Unnecessary Caching

--- __tests__/api/MetricsApi.test.ts
+++ __tests__/api/MetricsApi.test.ts
@@ -19,30 +19,26 @@ describe('MetricsApi.ts module', () => {
     it('should return the metric with the expected metric shape', async () => {
       // TODO: Test different metrics with different parameter types (conversion and
       // revenue). Can't do it now because only one metric is available to test.
-      const metrics = await MetricsApi.findById([31])
-      expect(metrics).toBeDefined()
-      expect(Array.isArray(metrics)).toBe(true)
-      expect(metrics.length).toBeGreaterThan(0)
-      metrics.forEach((metric) => {
-        expect(typeof metric.metricId).toBe('number')
-        expect(typeof metric.name).toBe('string')
-        expect(typeof metric.description).toBe('string')
-        expect(typeof metric.higherIsBetter).toBe('boolean')
-        expect(Array.isArray(metric.eventParams)).toBe(true)
-        metric.eventParams?.forEach((eventParam) => {
-          expect(typeof eventParam.event).toBe('string')
-          expect(typeof eventParam.props).toBe('object')
-          expect(eventParam.props).not.toBe(null)
-        })
-        expect(metric.revenueParams).toBe(null)
+      const metric = await MetricsApi.findById(31)
+      expect(metric).toBeDefined()
+      expect(typeof metric.metricId).toBe('number')
+      expect(typeof metric.name).toBe('string')
+      expect(typeof metric.description).toBe('string')
+      expect(typeof metric.higherIsBetter).toBe('boolean')
+      expect(Array.isArray(metric.eventParams)).toBe(true)
+      metric.eventParams?.forEach((eventParam) => {
+        expect(typeof eventParam.event).toBe('string')
+        expect(typeof eventParam.props).toBe('object')
+        expect(eventParam.props).not.toBe(null)
       })
+      expect(metric.revenueParams).toBe(null)
     })
 
-    it('called multiple times with same ID should return a cached metric', async () => {
-      const metrics1 = await MetricsApi.findById([31])
-      const metrics2 = await MetricsApi.findById([31])
-
-      expect(metrics1[0]).toBe(metrics2[0])
+    // TODO: Unskip this once the mock API stops returning the mock metric regardless
+    // of the given ID.
+    it.skip('called with an unknown metric ID should ', async () => {
+      const metric = await MetricsApi.findById(0)
+      expect(metric).not.toBeDefined()
     })
   })
 })
