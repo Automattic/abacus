6c84fe2db814be5e0f4f1c0d1547095156f8ab53
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-10 19:57:21 -0600

    Remove Out-of-Scope Stuff

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import AnalysesApi from './AnalysesApi'
-import ExperimentsApi from './ExperimentsApi'
-import MetricsApi from './MetricsApi'
-import SegmentsApi from './SegmentsApi'
-
-export { AnalysesApi, ExperimentsApi, MetricsApi, SegmentsApi }
