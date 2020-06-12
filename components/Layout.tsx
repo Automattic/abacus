96309e067d099a349aff34e3f1ccf4d05037e300
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-11 22:48:40 -0600

    Style App Header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import AppBar from '@material-ui/core/AppBar'
 import Container from '@material-ui/core/Container'
 import Head from 'next/head'
 import Link from 'next/link'
@@ -15,37 +16,43 @@ const Layout = ({ title, error, children }: { title: string; error?: Error | nul
       return renderError ? (
         <RenderErrorView renderError={renderError} />
       ) : (
-        <>
+        <div className='app-content'>
           <Head>
             <title>{title} | Abacus</title>
             <meta charSet='utf-8' />
             <meta name='viewport' content='initial-scale=1.0, width=device-width' />
           </Head>
-          <header>
-            <Container>
-              <nav>
-                <Link href='/'>
-                  <a>Experiments</a>
-                </Link>
-                <span>|</span>
-                <Link href='/metrics'>
-                  <a>Metrics</a>
-                </Link>
-              </nav>
-            </Container>
-          </header>
+          <AppBar color='inherit' position='relative'>
+            <div className='top'>
+              <Container maxWidth='xl'>
+                <span className='app-name'>Abacus</span>
+              </Container>
+            </div>
+            <div className='bottom'>
+              <Container maxWidth='xl'>
+                <nav>
+                  <Link href='/'>
+                    <a>Experiments</a>
+                  </Link>
+                  <span>|</span>
+                  <Link href='/metrics'>
+                    <a>Metrics</a>
+                  </Link>
+                </nav>
+              </Container>
+            </div>
+          </AppBar>
           <Container>
             <h1>{title}</h1>
             {error && <ErrorsBox errors={[error]} />}
             {children}
           </Container>
           <footer>
-            <hr />
             <Container>
               <span>The Abacus footer, brought to you by Automattic</span>
             </Container>
           </footer>
-        </>
+        </div>
       )
     }}
   </RenderErrorBoundary>
