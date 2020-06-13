66fe45185eed962658f69a734225518e8bdcfa26
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-12 18:15:50 -0600

    Add Comment

--- components/DatetimeText.tsx
+++ components/DatetimeText.tsx
@@ -6,6 +6,11 @@ const ISO_DATE_LENGTH = 10
  * Renders the date value in ISO 8601 format UTC.
  */
 const DatetimeText = ({ datetime, excludeTime }: { datetime: Date; excludeTime?: boolean }) => {
+  // In order to force a consistent locale and timezone for the unit tests, we set
+  // the following env vars. In the browser, we don't have these set and the function
+  // behaves as if no parameters were passed to it. Note: Setting the env vars and
+  // not explicit setting them here works in non-Windows environments. We are only
+  // being explicit here because of Windows.
   const datetimeText = datetime.toLocaleString(process.env.LANG, { timeZone: process.env.TZ })
   let text = datetime.toISOString()
   if (excludeTime) {
