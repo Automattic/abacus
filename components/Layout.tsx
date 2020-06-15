707a055f5f248bacb0817f1d2e967901b616d70a
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-14 18:43:37 -0600

    Style Logo Via Stylesheet

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -25,7 +25,7 @@ const Layout = ({ title, error, children }: { title: string; error?: Error | nul
           <AppBar color='inherit' position='relative'>
             <div className='top'>
               <Container maxWidth='xl'>
-                <img alt='logo' className='mr-1' src='/img/logo.png' width='24' />
+                <img alt='logo' className='app-logo' src='/img/logo.png' />
                 <span className='app-name'>Abacus</span>
               </Container>
             </div>
