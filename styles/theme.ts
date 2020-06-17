995bf0b7d30486e744bba3a86809979d14c8838b
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-16 22:08:16 -0600

    Extend Background Color Palette for Errors

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,11 +1,30 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
+declare module '@material-ui/core/styles/createPalette' {
+  interface TypeBackground {
+    error: React.CSSProperties['color']
+  }
+
+  interface Palette {
+    background: Palette['background']
+  }
+
+  interface PaletteOptions {
+    background?: PaletteOptions['background']
+  }
+}
+
 const theme = createMuiTheme({
   overrides: {
     MuiAppBar: {
       colorPrimary: {
         backgroundColor: '#194661',
       },
+    },
+  },
+  palette: {
+    background: {
+      error: '#f8d7da',
     },
   },
 })
