7f6292ae20924b3eec0a0efffe620ee23a53f3d4
Jessie Ross <jessie.ross@automattic.com>
2020-06-22 16:48:26 +0800

    Add conversion parameter type to MetricTable storybook

--- components/MetricsTable.stories.tsx
+++ components/MetricsTable.stories.tsx
@@ -13,11 +13,6 @@ const metrics: MetricBare[] = []
 
 export const withNoMetrics = () => <MetricsTable metrics={metrics} />
 
-const METRIC_TEMPLATE = {
-  description: 'The description.',
-  parameterType: 'revenue' as 'revenue',
-}
-
 let idCounter = 0
 const newId = (): number => {
   const thisId = idCounter
@@ -28,10 +23,11 @@ const newId = (): number => {
 const randomMetric = (): MetricBare => {
   const id = newId()
   return {
-    ...METRIC_TEMPLATE,
     // ts-ignore: don't know what is wrong with this fella
     metricId: id,
     name: `Name: ${id}`,
+    description: 'The description for ${id}.',
+    parameterType: id % 2 === 0 ? 'revenue' : 'conversion',
   } as MetricBare
 }
 
