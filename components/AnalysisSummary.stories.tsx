a41ebc549ee60b05d111f733f82e189c15a2f394
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-22 21:19:43 -0600

    Remove Space Introduced in a Prev Commit

--- components/AnalysisSummary.stories.tsx
+++ components/AnalysisSummary.stories.tsx
@@ -11,9 +11,7 @@ const experiment = Fixtures.createExperimentFull()
 const metrics = Fixtures.createMetricsBares()
 
 export const noAnalyses = () => <AnalysisSummary analyses={[]} experiment={experiment} metrics={metrics} />
-
 export const someAnalyses = () => <AnalysisSummary analyses={analyses} experiment={experiment} metrics={metrics} />
-
 export const someAnalysesDebugMode = () => (
   <AnalysisSummary analyses={analyses} experiment={experiment} metrics={metrics} debugMode={true} />
 )
