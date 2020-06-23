9a26db3c4ed06e77a9e2466d9185c375bdde4cb4
Jessie Ross <jessie.ross@automattic.com>
2020-06-23 14:57:06 +0800

    Use fixture data for MetricTable tests

--- components/MetricsTable.test.tsx
+++ components/MetricsTable.test.tsx
@@ -1,13 +1,12 @@
 import { getByText, render } from '@testing-library/react'
 import React from 'react'
 
-import { MetricBare } from '@/models/MetricBare'
+import Fixtures from '@/helpers/fixtures'
 
 import MetricsTable from './MetricsTable'
 
 test('with no metrics, renders an empty table', () => {
-  const metrics: MetricBare[] = []
-  const { container, getByText } = render(<MetricsTable metrics={metrics} />)
+  const { container, getByText } = render(<MetricsTable metrics={[]} />)
 
   expect(getByText('Name')).toBeInTheDocument()
   expect(getByText('Description')).toBeInTheDocument()
@@ -19,26 +18,12 @@ test('with no metrics, renders an empty table', () => {
 })
 
 test('with some metrics, renders a table', () => {
-  const metrics: MetricBare[] = [
-    {
-      metricId: 1,
-      name: 'name 1',
-      description: 'description 1',
-      parameterType: 'revenue',
-    },
-    {
-      metricId: 2,
-      name: 'name 2',
-      description: 'description 2',
-      parameterType: 'conversion',
-    },
-  ]
-  const { container } = render(<MetricsTable metrics={metrics} />)
+  const { container } = render(<MetricsTable metrics={Fixtures.createMetricsBares(2)} />)
 
   const tBodyElmt = container.querySelector('tbody') as HTMLTableSectionElement
   expect(tBodyElmt).not.toBeNull()
-  expect(getByText(tBodyElmt, 'name 1', { selector: 'tr > td' })).toBeInTheDocument()
-  expect(getByText(tBodyElmt, 'description 1', { selector: 'tr > td' })).toBeInTheDocument()
+  expect(getByText(tBodyElmt, 'metric_1', { selector: 'tr > td' })).toBeInTheDocument()
+  expect(getByText(tBodyElmt, 'This is metric 1', { selector: 'tr > td' })).toBeInTheDocument()
   expect(getByText(tBodyElmt, 'revenue', { selector: 'tr > td' })).toBeInTheDocument()
   expect(getByText(tBodyElmt, 'conversion', { selector: 'tr > td' })).toBeInTheDocument()
 })
