aee58cae38250c38781d65cf3f3bd8e7ef6e126f
Jessie Ross <jessie.ross@automattic.com>
2020-06-26 16:16:10 +0800

    Simplify table features for MVP

--- utils/material-table.ts
+++ utils/material-table.ts
@@ -1,7 +1,10 @@
 export const defaultTableOptions = {
   emptyRowsWhenPaging: false,
   pageSize: 25,
-  pageSizeOptions: [25, 50, 100],
   showEmptyDataSourceMessage: false,
   showTitle: false,
+  search: false,
+  toolbar: false,
+  pageSizeOptions: [],
+  showFirstLastPageButtons: false,
 }
