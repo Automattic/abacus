ec51afe7724e558c4a9e33de29473b82131977d7
Jessie Ross <jessie.ross@automattic.com>
2020-06-26 16:10:58 +0800

    Polish ExperimentStatus

--- components/ExperimentStatus.tsx
+++ components/ExperimentStatus.tsx
@@ -1,35 +1,43 @@
-import { makeStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
 import clsx from 'clsx'
 import React from 'react'
 
 import { Status } from '@/models'
 
-const useStatusStyles = makeStyles({
-  root: {
-    borderRadius: 1,
-    padding: '0.5rem',
-  },
-  completed: {
-    background: '#4caf5014',
-    color: '#4caf50',
-  },
-  disabled: {
-    background: '#82828214',
-    color: '#828282',
-  },
-  running: {
-    background: '#ff980014',
-    color: '#ff9800',
-  },
-  staging: {
-    background: '#82828214',
-    color: '#828282',
-  },
-})
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      padding: '3px 5px',
+      borderRadius: 3,
+      textTransform: 'uppercase',
+    },
+    completed: {
+      color: '#4CAF50',
+      background: 'rgba(76, 175, 80, 0.08)',
+    },
+    running: {
+      color: '#FF9800',
+      background: 'rgba(255, 152, 0, 0.08)',
+    },
+    staging: {
+      color: '#1E77A5',
+      background: 'rgba(30, 119, 165, 0.08)',
+    },
+    disabled: {
+      color: '#828282',
+      background: 'rgba(130, 130, 130, 0.08)',
+    },
+  }),
+)
 
 function ExperimentStatus({ status }: { status: Status }) {
-  const classes = useStatusStyles()
-  return <span className={clsx(classes.root, classes[status])}>{status}</span>
+  const classes = useStyles()
+  return (
+    <Typography className={clsx(classes.root, classes[status])} variant='caption'>
+      {status}
+    </Typography>
+  )
 }
 
 export default ExperimentStatus
