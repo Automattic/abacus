d134d89cc89f64e3549141290e20f023b479993d
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2020-06-27 13:06:39 -0600

    Add Responsive Layout for Experiment Details

--- components/ExperimentDetails.test.tsx
+++ components/ExperimentDetails.test.tsx
@@ -2,10 +2,34 @@ import { render } from '@testing-library/react'
 import React from 'react'
 
 import Fixtures from '@/helpers/fixtures'
+import { createMatchMedia } from '@/helpers/test-utils'
 
 import ExperimentDetails from './ExperimentDetails'
 
-test('renders as expected', () => {
+const initialJsDomWindowInnerWidth = window.innerWidth
+afterAll(() => {
+  window.matchMedia = createMatchMedia(initialJsDomWindowInnerWidth)
+})
+
+test('renders as expected at large width', () => {
+  window.matchMedia = createMatchMedia(1600)
+
+  const metrics = Fixtures.createMetricBares()
+  const segments = Fixtures.createSegments(5)
+  const experiment = Fixtures.createExperimentFull({
+    segmentAssignments: [
+      Fixtures.createSegmentAssignment({ segmentAssignmentId: 101, segmentId: 1 }),
+      Fixtures.createSegmentAssignment({ segmentAssignmentId: 102, segmentId: 2, isExcluded: true }),
+    ],
+  })
+  const { container } = render(<ExperimentDetails experiment={experiment} metrics={metrics} segments={segments} />)
+
+  expect(container).toMatchSnapshot()
+})
+
+test('renders as expected at small width', () => {
+  window.matchMedia = createMatchMedia(600)
+
   const metrics = Fixtures.createMetricBares()
   const segments = Fixtures.createSegments(5)
   const experiment = Fixtures.createExperimentFull({
