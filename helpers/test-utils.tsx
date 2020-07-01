45eb743c58aa0d60080c4b9f6d159e2be1ac28c2
Jessie Ross <jessie.ross@automattic.com>
2020-07-01 16:48:52 +0800

    JSDOC test-utils.tsx:render

--- helpers/test-utils.tsx
+++ helpers/test-utils.tsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 import ThemeProvider from '@/styles/ThemeProvider'
 
+/**
+ * A wrapped unit-test react-renderer, useful for adding React Contexts globally.
+ */
 export const render: typeof actualRender = <Q extends Queries>(ui: React.ReactElement, options?: RenderOptions<Q>) =>
   actualRender((<ThemeProvider>{ui}</ThemeProvider>) as React.ReactElement, options) as ReturnType<typeof actualRender>
