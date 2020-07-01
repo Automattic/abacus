9c521cd625f90c4a849a97aee451ed62ca30d966
Jessie Ross <jessie.ross@automattic.com>
2020-07-02 01:21:58 +0800

    Stub ExCreation beginning form-part

---
+++ components/experiment-creation/form-parts/Beginning.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import { Typography, TextField, Button } from '@material-ui/core'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+    },
+  }),
+)
+
+const Beginning = ({ /* TODO: PROPS */ }: { /* TODO: PROPS  */ }) => {
+  const classes = useStyles()
+
+  return (<>
+  </>)
+}
+
+export default Beginning
