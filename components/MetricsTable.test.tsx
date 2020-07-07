d47491c8fd5725185815769854b67cd38c4f9e93
Jessie Ross <jessie.ross@automattic.com>
2020-07-07 22:48:03 +0800

    Add unit tests for MetricsDetail

--- components/MetricsTable.test.tsx
+++ components/MetricsTable.test.tsx
@@ -1,10 +1,15 @@
 import { fireEvent, getByText, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 
+import MetricsApi from '@/api/MetricsApi'
 import Fixtures from '@/helpers/fixtures'
 import { render } from '@/helpers/test-utils'
 
 import MetricsTable from './MetricsTable'
+
+jest.mock('@/api/MetricsApi')
+
+const mockedMetricsApi = (MetricsApi as unknown) as jest.Mocked<typeof MetricsApi>
 
 test('with no metrics, renders an empty table', () => {
   const { container, getByText } = render(<MetricsTable metrics={[]} />)
@@ -18,7 +23,7 @@ test('with no metrics, renders an empty table', () => {
   expect(tBodyElmt).toHaveTextContent('')
 })
 
-test('with some metrics, renders a table', async () => {
+test('with some metrics, renders a table', () => {
   const { container } = render(<MetricsTable metrics={Fixtures.createMetricBares(2)} />)
 
   const tBodyElmt = container.querySelector('tbody') as HTMLTableSectionElement
@@ -27,7 +32,28 @@ test('with some metrics, renders a table', async () => {
   expect(getByText(tBodyElmt, 'This is metric 1', { selector: 'tr > td' })).toBeInTheDocument()
   expect(getByText(tBodyElmt, 'revenue', { selector: 'tr > td' })).toBeInTheDocument()
   expect(getByText(tBodyElmt, 'conversion', { selector: 'tr > td' })).toBeInTheDocument()
+})
+
+test('with some metrics, displays an error on trouble loading metric details', async () => {
+  mockedMetricsApi.findById.mockRejectedValue(new Error())
+
+  const { container } = render(<MetricsTable metrics={Fixtures.createMetricBares(2)} />)
+
+  const tBodyElmt = container.querySelector('tbody') as HTMLTableSectionElement
+  expect(tBodyElmt).not.toBeNull()
 
   fireEvent.click(screen.getByText('metric_1'))
   await waitFor(() => screen.getByText('Oops! Something went wrong while trying to load a Metric.'))
+})
+
+test('with some metrics, loads and opens metric details', async () => {
+  mockedMetricsApi.findById.mockResolvedValue(Fixtures.createMetricFull(0))
+
+  const { container } = render(<MetricsTable metrics={Fixtures.createMetricBares(2)} />)
+
+  const tBodyElmt = container.querySelector('tbody') as HTMLTableSectionElement
+  expect(tBodyElmt).not.toBeNull()
+
+  fireEvent.click(screen.getByText('metric_1'))
+  await waitFor(() => screen.getByText('Higher is Better'))
 })
