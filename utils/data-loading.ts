715adb14109c2a869ac5995c78c0284f1dd45199
Jessie Ross <jessie.ross@automattic.com>
2020-07-08 13:13:17 +0800

    Add useDataLoadingError hook

---
+++ utils/data-loading.ts
@@ -0,0 +1,15 @@
+import { useSnackbar } from 'notistack'
+import { useEffect } from 'react'
+
+export function useDataLoadingError<E extends Error | null>(error: E, dataName?: string) {
+  const { enqueueSnackbar } = useSnackbar()
+
+  useEffect(() => {
+    if (error) {
+      const userErrorMessage = dataName
+        ? `Oops! There was a problem loading some data: ${dataName}`
+        : 'Oops! There was a problem loading data'
+      enqueueSnackbar(userErrorMessage)
+    }
+  }, [error])
+}
