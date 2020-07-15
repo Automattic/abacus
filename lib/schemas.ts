a8b7b3029b4c034ff75495ed86d64301205af8c7
Jessie Ross <jessie.ross@automattic.com>
2020-07-15 12:21:28 +0800

    Remove lingering schema docs

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -134,22 +134,11 @@ export const variationSchema = yup
   .camelCase()
 export type Variation = yup.InferType<typeof variationSchema>
 
-/**
- * The platform where the experiment is running.
- * - `calypso`: The experiment is being run on the front-end Calypso interface,
- *   WordPress.com. Account sign-up and site management design experiments are
- *   likely run here.
- * - `wpcom`: The experiment is being run on the back-end, like APIs which are
- *   usually written in PHP. Email and landing pages experiments are likely run here.
- */
 export enum Platform {
   Calypso = 'calypso',
   Wpcom = 'wpcom',
 }
 
-/**
- * The status of an experiment.
- */
 export enum Status {
   Staging = 'staging',
   Running = 'running',
