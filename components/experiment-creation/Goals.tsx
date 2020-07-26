7a92559c56a28f6280f69b168795608f7e675f93
Jessie Ross <jessie.ross@automattic.com>
2020-07-26 18:21:38 +0800

    Implement assigning metrics

--- components/experiment-creation/Goals.tsx
+++ components/experiment-creation/Goals.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import _ from 'lodash'
 import {
   Typography,
   TableContainer,
@@ -16,7 +17,14 @@ import {
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import React, { useState } from 'react'
 import { FieldArray, useField } from 'formik'
-import { MetricAssignment, MetricBare, MetricParameterType } from '@/lib/schemas'
+import {
+  MetricAssignment,
+  MetricBare,
+  MetricParameterType,
+  MetricAssignmentNew,
+  AttributionWindowSeconds,
+} from '@/lib/schemas'
+import { toIntOrNull } from 'qc-to_int'
 
 const normalizedMetrics: Record<number, MetricBare> = {
   1: {
@@ -26,7 +34,7 @@ const normalizedMetrics: Record<number, MetricBare> = {
     parameterType: MetricParameterType.Revenue,
   },
   2: {
-    metricId: 1,
+    metricId: 2,
     name: 'registration_start',
     description: 'string',
     parameterType: MetricParameterType.Conversion,
@@ -52,18 +60,30 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 )
 
+const createMetricAssignment = (metric: MetricBare) => {
+  return {
+    metricId: metric.metricId,
+    attributionWindowSeconds: AttributionWindowSeconds.TwentyFourHours,
+    isPrimary: false,
+    changeExpected: null,
+    minDifference: null,
+  }
+}
+
 const Goals = () => {
   const classes = useStyles()
 
   const [metricAssignmentsField] = useField<MetricAssignment[]>('experiment.metricAssignments')
-
   const [selectedMetricId, setSelectedMetricId] = useState<string>('')
   const onSelectedMetricChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     setSelectedMetricId(event.target.value as string)
   }
-  const onAddMetric = () => {
-    setSelectedMetricId('')
-  }
+
+  const usedMetricIds = metricAssignmentsField.value.map((metricAssignment) => metricAssignment.metricId)
+  const avaliableMetricIds = _.difference(
+    Object.keys(normalizedMetrics).map((id) => parseInt(id, 10)),
+    usedMetricIds,
+  )
 
   return (
     <div className={classes.root}>
@@ -71,81 +91,94 @@ const Goals = () => {
         Goals
       </Typography>
 
-      <div className={classes.addMetric}>
-        <Typography variant='h5' gutterBottom>
-          Assign a Metric
-        </Typography>
-        <Typography variant='subtitle2' gutterBottom>
-          Quantify the impact you're trying to measure
-        </Typography>
-        <div className={classes.addMetricControls}>
-          <FormControl className={classes.addMetricSelect}>
-            <InputLabel id='add-metric-label'>Search Metrics</InputLabel>
-            <Select
-              labelId='add-metric-label'
-              id='add-metric-select'
-              value={selectedMetricId}
-              onChange={onSelectedMetricChange}
-              displayEmpty
-            >
-              {Object.values(normalizedMetrics).map((metric) => (
-                <MenuItem value={metric.metricId} key={metric.metricId}>
-                  {metric.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <Button variant='outlined' size='small' onClick={onAddMetric}>
-            Add
-          </Button>
-        </div>
-      </div>
+      <FieldArray
+        name='experiment.metricAssignments'
+        render={(arrayHelpers) => {
+          const onAddMetric = () => {
+            const metricId = parseInt(selectedMetricId, 10)
+            const metric = normalizedMetrics[metricId]
+            if (metricId) {
+              arrayHelpers.push(createMetricAssignment(metric))
+            }
+            setSelectedMetricId('')
+          }
 
-      <Typography variant='h5' gutterBottom>
-        Assigned Metrics
-      </Typography>
-      <Typography variant='subtitle2' gutterBottom>
-        Configure your metrics
-      </Typography>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Metric</TableCell>
-              <TableCell>Attribution Window (Seconds)</TableCell>
-              <TableCell>Change Expected</TableCell>
-              <TableCell>Minimum Difference</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <FieldArray
-              name='experiment.metricAssignments'
-              render={(arrayHelpers) => (
-                <>
-                  {metricAssignmentsField.value.map((metricAssignment, index) => (
+          return (
+            <>
+              <div className={classes.addMetric}>
+                <Typography variant='h5' gutterBottom>
+                  Assign a Metric
+                </Typography>
+                <Typography variant='subtitle2' gutterBottom>
+                  Quantify the impact you're trying to measure
+                </Typography>
+                <div className={classes.addMetricControls}>
+                  <FormControl className={classes.addMetricSelect}>
+                    <InputLabel id='add-metric-label'>Search Metrics</InputLabel>
+                    <Select
+                      labelId='add-metric-label'
+                      id='add-metric-select'
+                      value={selectedMetricId}
+                      onChange={onSelectedMetricChange}
+                      displayEmpty
+                    >
+                      {avaliableMetricIds
+                        .map((metricId) => normalizedMetrics[metricId])
+                        .map((metric) => (
+                          <MenuItem value={metric.metricId} key={metric.metricId}>
+                            {metric.name}
+                          </MenuItem>
+                        ))}
+                    </Select>
+                  </FormControl>
+                  <Button variant='outlined' size='small' onClick={onAddMetric}>
+                    Add
+                  </Button>
+                </div>
+              </div>
+
+              <Typography variant='h5' gutterBottom>
+                Assigned Metrics
+              </Typography>
+              <Typography variant='subtitle2' gutterBottom>
+                Configure your metrics
+              </Typography>
+              <TableContainer>
+                <Table>
+                  <TableHead>
                     <TableRow>
-                      <TableCell>{normalizedMetrics[metricAssignment.metricId]}</TableCell>
-                      <TableCell>60</TableCell>
-                      <TableCell>Yes</TableCell>
-                      <TableCell>10%</TableCell>
+                      <TableCell>Metric</TableCell>
+                      <TableCell>Attribution Window (Seconds)</TableCell>
+                      <TableCell>Change Expected</TableCell>
+                      <TableCell>Minimum Difference</TableCell>
                     </TableRow>
-                  ))}
-                </>
-              )}
-            />
-            <TableRow>
-              <TableCell colSpan={4}>
-                {metricAssignmentsField.value.length === 0 && (
-                  <Typography variant='body1' align='center'>
-                    {' '}
-                    You don't have any metrics yet.
-                  </Typography>
-                )}
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
+                  </TableHead>
+                  <TableBody>
+                    {metricAssignmentsField.value.map((metricAssignment, index) => (
+                      <TableRow key={metricAssignment.metricId}>
+                        <TableCell>{normalizedMetrics[metricAssignment.metricId].name}</TableCell>
+                        <TableCell>60</TableCell>
+                        <TableCell>Yes</TableCell>
+                        <TableCell>10%</TableCell>
+                      </TableRow>
+                    ))}
+                    {metricAssignmentsField.value.length === 0 && (
+                      <TableRow>
+                        <TableCell colSpan={4}>
+                          <Typography variant='body1' align='center'>
+                            {' '}
+                            You don't have any metrics yet.
+                          </Typography>
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </>
+          )
+        }}
+      />
     </div>
   )
 }
