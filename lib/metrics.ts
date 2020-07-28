afaad46fd3f9761f98753af2deae478eb46f82f8
Jessie Ross <jessie.ross@automattic.com>
2020-07-28 19:58:33 +0800

    Depluralise USD

--- lib/metrics.ts
+++ lib/metrics.ts
@@ -1,5 +1,5 @@
 import { MetricBare } from './schemas'
 
 export function getUnit<Metric extends MetricBare>(metric: Metric) {
-  return metric.parameterType === 'conversion' ? 'conversions' : 'USDs'
+  return metric.parameterType === 'conversion' ? 'conversions' : 'USD'
 }
