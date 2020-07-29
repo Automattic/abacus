11d93f114982d22f0c94276d613caba3fdc9879e
Jessie Ross <jessie.ross@automattic.com>
2020-07-29 22:28:44 +0800

    Use pp for metric conversions unit

--- lib/metrics.ts
+++ lib/metrics.ts
@@ -1,5 +1,5 @@
 import { MetricBare } from './schemas'
 
 export function getUnit<Metric extends MetricBare>(metric: Metric) {
-  return metric.parameterType === 'conversion' ? 'conversions' : 'USD'
+  return metric.parameterType === 'conversion' ? 'pp' : 'USD'
 }
