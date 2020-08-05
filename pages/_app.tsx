bdd93059710827315636fa0bdb66a3dadd0a0da0
Jessie Ross <jessie.ross@automattic.com>
2020-08-05 09:17:05 +0800

    Fix auth flow for mock server

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,9 @@ import * as yup from 'yup'
 
 import RenderErrorBoundary from '@/components/RenderErrorBoundary'
 import RenderErrorView from '@/components/RenderErrorView'
+import { config } from '@/config'
 import ThemeProvider from '@/styles/ThemeProvider'
 import { getExperimentsAuthInfo } from '@/utils/auth'
-
-import { config } from '../config'
 
 const debug = debugFactory('abacus:pages/_app.tsx')
 
@@ -94,7 +93,7 @@ const App = React.memo(function App(props: AppProps) {
     }
   }, [])
 
-  if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined' && config.experimentApi.needsAuth) {
     // Prompt user for authorization if we don't have auth info.
     const experimentsAuthInfo = getExperimentsAuthInfo()
     if (!experimentsAuthInfo) {
