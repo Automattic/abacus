891149d8fb79cc13b9eb7d226211d29ffc8f4d8e
Jessie Ross <jessie.ross@automattic.com>
2020-08-20 16:07:16 +0800

    Add edit metric button

--- pages/metrics/index.tsx
+++ pages/metrics/index.tsx
@@ -6,16 +6,31 @@ import MetricsApi from '@/api/MetricsApi'
 import Layout from '@/components/Layout'
 import MetricsTable from '@/components/MetricsTable'
 import { useDataLoadingError, useDataSource } from '@/utils/data-loading'
+import { useRouter } from 'next/router'
 
 const debug = debugFactory('abacus:pages/metrics/index.tsx')
 
 const MetricsIndexPage = () => {
   debug('MetricsIndexPage#render')
   const { isLoading, data: metrics, error } = useDataSource(() => MetricsApi.findAll(), [])
+  const router = useRouter()
+  const debugMode = router.query.debug === 'true'
 
   useDataLoadingError(error, 'Metrics')
 
-  return <Layout title='Metrics'>{isLoading ? <LinearProgress /> : <MetricsTable metrics={metrics || []} />}</Layout>
+  const onEditMetric = (metricId: number) => alert(metricId)
+
+  return (
+    <Layout title='Metrics'>
+      {isLoading
+        ? <LinearProgress />
+        : <MetricsTable
+          canEditMetrics={debugMode}
+          metrics={metrics || []}
+          onEditMetric={onEditMetric}
+        />}
+    </Layout>
+  )
 }
 
 export default MetricsIndexPage
