ac76ea0ea9fbd3d84d11cf8669c47c9e92e990fe
Jessie Ross <jessie.ross@automattic.com>
2020-08-21 11:01:20 +0800

    Remove canEditMetric

--- pages/metrics/index.tsx
+++ pages/metrics/index.tsx
@@ -39,7 +39,7 @@ const MetricsIndexPage = () => {
         <LinearProgress />
       ) : (
         <>
-          <MetricsTable canEditMetrics={debugMode} metrics={metrics || []} onEditMetric={onEditMetric} />
+          <MetricsTable metrics={metrics || []} onEditMetric={onEditMetric} />
           {debugMode && (
             <div className={classes.actions}>
               <Button variant='contained' color='secondary' onClick={onAddMetric}>
