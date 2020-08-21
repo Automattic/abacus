77b93203c02f29f5379b687a7aead3a5a008438e
Jessie Ross <j@scjr.me>
2020-08-21 13:32:05 +0800

    Update index.tsx

--- pages/metrics/index.tsx
+++ pages/metrics/index.tsx
@@ -39,7 +39,7 @@ const MetricsIndexPage = () => {
         <LinearProgress />
       ) : (
         <>
-          <MetricsTable metrics={metrics || []} onEditMetric={debugMode && onEditMetric} />
+          <MetricsTable metrics={metrics || []} onEditMetric={debugMode ? onEditMetric : undefined} />
           {debugMode && (
             <div className={classes.actions}>
               <Button variant='contained' color='secondary' onClick={onAddMetric}>
