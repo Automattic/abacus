02d28df4569a613c0686e568da262f7be5dc9060
Rob Landers <landers.robert@gmail.com>
2020-09-30 15:09:42 +0200

    Create autocomplete component

---
+++ components/Autocomplete.tsx
@@ -0,0 +1,56 @@
+import { CircularProgress } from '@material-ui/core'
+import { Autocomplete } from '@material-ui/lab'
+import { AutocompleteProps, AutocompleteRenderInputParams, fieldToAutocomplete } from 'formik-material-ui-lab'
+import React, { useCallback } from 'react'
+
+import { AutocompleteItem } from '@/lib/schemas'
+
+export interface AutocompleteAttributes {
+  renderInput: (params: AutocompleteRenderInputParams) => unknown
+  getOptionSelected: (option: AutocompleteItem, value: string | AutocompleteItem) => boolean
+  options: AutocompleteItem[]
+  loading: boolean
+  getOptionLabel: (option: AutocompleteItem | string | null) => string
+}
+
+export const getOptionValue = (option: AutocompleteItem | string | null) =>
+  (typeof option === 'string' ? option : option?.value) ?? ''
+
+export const getOptionLabel = (option: AutocompleteItem | string | null) =>
+  (typeof option === 'string' ? option : option?.name) ?? ''
+
+export const getOptionSelected = (option: AutocompleteItem, value: string | AutocompleteItem) =>
+  typeof value === 'string' ? value === option.value : value.value === option.value
+
+export const autocompleteAttributes = {
+  getOptionLabel: getOptionLabel,
+  getOptionSelected: getOptionSelected,
+}
+
+export const autocompleteInputProps = (params: AutocompleteRenderInputParams, loading: boolean) => {
+  return {
+    ...params.InputProps,
+    endAdornment: (
+      <>
+        {loading ? <CircularProgress color='inherit' size={20} /> : null}
+        {params.InputProps.endAdornment}
+      </>
+    ),
+  }
+}
+
+export default function AbacusAutocomplete(props: AutocompleteProps<AutocompleteItem, false, false, false>) {
+  const {
+    form: { setFieldValue },
+    field: { name },
+  } = props
+
+  const onChange = useCallback(
+    (ev, option: AutocompleteItem | string | null) => {
+      setFieldValue(name, getOptionValue(option))
+    },
+    [setFieldValue, name],
+  )
+
+  return <Autocomplete {...fieldToAutocomplete(props)} {...autocompleteAttributes} onChange={onChange} />
+}
