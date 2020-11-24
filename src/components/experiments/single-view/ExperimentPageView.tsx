5775174539e5ba5093ded5da87eb55abb23636a2
Jessie Ross <jessie.ross@automattic.com>
2020-11-24 12:14:35 +0800

    Invert from the top

--- src/components/experiments/single-view/ExperimentPageView.tsx
+++ src/components/experiments/single-view/ExperimentPageView.tsx
@@ -110,7 +110,29 @@ export default function ExperimentPageView({
   useDataLoadingError(tagsError, 'Tags')
 
   const { isLoading: analysesIsLoading, data: analyses, error: analysesError } = useDataSource(
-    () => (experimentId ? AnalysesApi.findByExperimentId(experimentId) : createUnresolvingPromise<Analysis[]>()),
+    async () => {
+      if (!experimentId) {
+        return createUnresolvingPromise<Analysis[]>()
+      } 
+      const analyses = await AnalysesApi.findByExperimentId(experimentId)
+
+      // NOTE: Estimates are the wrong way round coming from the backend so we invert them here as a workaround
+      // until that is fixed.
+      // TODO: Fix in backend.
+      return analyses.map(analysis => ({
+        ...analysis,
+        metricEstimates: {
+          ...analysis.metricEstimates,
+          ...(analysis.metricEstimates?.diff && ({
+            diff: {
+              top: -1 * analysis.metricEstimates.diff.top,
+              estimate: -1 * analysis.metricEstimates.diff.estimate,
+              bottom: -1 * analysis.metricEstimates.diff.bottom,
+            }
+          }))
+        },
+      }))
+    },
     [experimentId],
   )
   useDataLoadingError(analysesError, 'Analyses')
