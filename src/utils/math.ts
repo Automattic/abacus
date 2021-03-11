36dda5cabb5cb59254413f80a54f3813f043e89e
Jessie Ross <jessie.ross@automattic.com>
2021-03-11 11:05:50 +0800

    Move the abs

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -24,8 +24,8 @@ export function binomialProbValue({
   // (We don't care about the accuracy for n <= 30 so we let them be.)
   // And if Y ~ N(0, 1/2) then Y = (X - mean)/(2 * variance)^(1/2)
   // See also: https://en.wikipedia.org/wiki/Normal_distribution#Symmetries_and_derivatives
-  const y = abs(successfulTrials - mean) / Math.sqrt(2 * variance)
-  // Since, by definition, erf(y) gives the probabilty of [-y, y] on Y, and pValue is the union of [-inf, -y] and [y, inf]:
+  const y = (successfulTrials - mean) / Math.sqrt(2 * variance)
+  // Since erf(abs(y)) gives the probabilty of [-y, y] on Y, and pValue is the union of [-inf, -y] and [y, inf]:
   // See also: https://en.wikipedia.org/wiki/Error_function
-  return 1 - erf(y)
+  return 1 - erf(abs(y))
 }
