8317c9d43f044dc30bd7c3e97c79105888786dfc
Jessie Ross <jessie.ross@automattic.com>
2021-03-16 01:02:56 +0800

    Use stdlib for the binomial test

---
+++ src/utils/binomial-test.d.ts
@@ -0,0 +1,5 @@
+declare module '@stdlib/stats/binomial-test' {
+    export default (x: number, n: number, { p: number }) => {
+        pValue: number
+    }
+}
--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,4 +1,4 @@
-import { abs, erf } from 'mathjs'
+import binomialTest from '@stdlib/stats/binomial-test';
 
 /**
  * Assuming a distribution of X ~ Binomial(n, p), returns the probability of the number of trials being equal to x or more extreme than x.
@@ -16,16 +16,5 @@ export function binomialProbValue({
   totalTrials: number
   probabilityOfSuccess: number
 }): number {
-  const mean = totalTrials * probabilityOfSuccess
-  const variance = totalTrials * probabilityOfSuccess * (1 - probabilityOfSuccess)
-  // By the CLT, B ~ Binomial(n, p) is approximated well enough by X ~ N(mean, variance) for n > 30
-  // See also: https://en.wikipedia.org/wiki/Central_limit_theorem
-  //           https://en.wikipedia.org/wiki/Binomial_distribution#Normal_approximation
-  // (We don't care about the accuracy for n <= 30 so we let them be.)
-  // And if Y ~ N(0, 1/2) then Y = (X - mean)/(2 * variance)^(1/2)
-  // See also: https://en.wikipedia.org/wiki/Normal_distribution#Symmetries_and_derivatives
-  const y = (successfulTrials - mean) / Math.sqrt(2 * variance)
-  // Since erf(abs(y)) gives the probabilty of [-y, y] on Y, and pValue is the union of [-inf, -y] and [y, inf]:
-  // See also: https://en.wikipedia.org/wiki/Error_function
-  return 1 - erf(abs(y))
+  return binomialTest(successfulTrials, totalTrials, { p: probabilityOfSuccess }).pValue
 }
