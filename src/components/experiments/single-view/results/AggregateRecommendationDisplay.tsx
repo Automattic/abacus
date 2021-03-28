0991643c17eb9fec7ed69998441806c2aa4590c5
Jessie Ross <jessie.ross@automattic.com>
2021-03-29 01:56:25 +0800

    Improve aggregateRec decision display

--- src/components/experiments/single-view/results/AggregateRecommendationDisplay.tsx
+++ src/components/experiments/single-view/results/AggregateRecommendationDisplay.tsx
@@ -1,4 +1,5 @@
 import { createStyles, makeStyles, Theme, Tooltip } from '@material-ui/core'
+import clsx from 'clsx'
 import React from 'react'
 
 import { AggregateRecommendation, AggregateRecommendationDecision } from 'src/lib/analyses'
@@ -25,17 +26,28 @@ export default function AggregateRecommendationDisplay({
   experiment: ExperimentFull
 }): JSX.Element {
   const classes = useStyles()
+
   switch (aggregateRecommendation.decision) {
     case AggregateRecommendationDecision.ManualAnalysisRequired:
       return (
-        <Tooltip title='Contact @experimentation-review on #a8c-experiments'>
+        <Tooltip title='Contact @experimentation-review on #a8c-experiments.'>
           <span className={classes.tooltipped}>Manual analysis required</span>
         </Tooltip>
       )
     case AggregateRecommendationDecision.MissingAnalysis:
-      return <>Not analyzed yet</>
+      return (
+        <Tooltip title='It takes 24-48 hours for data to be analyzed.'>
+          <span className={classes.tooltipped}> Not analyzed yet </span>
+        </Tooltip>
+      )
+    case AggregateRecommendationDecision.MoreDataNeeded:
+      return <>More data needed</>
     case AggregateRecommendationDecision.Inconclusive:
-      return <>Inconclusive</>
+      return (
+        <Tooltip title='There was not enough data to make a conclusion.'>
+          <span className={classes.tooltipped}> Inconclusive </span>
+        </Tooltip>
+      )
     case AggregateRecommendationDecision.DeployAnyVariation:
       return <>Deploy either variation</>
     case AggregateRecommendationDecision.DeployChosenVariation: {
@@ -46,9 +58,9 @@ export default function AggregateRecommendationDisplay({
         throw new Error('No match for chosenVariationId among variations in experiment.')
       }
 
-      return <>Deploy {chosenVariation.name}</>
+      return <> Deploy {chosenVariation.name} </>
     }
     default:
-      throw new Error('Missing AggregateRecommendationDecision.')
+      throw new Error(`Missing AggregateRecommendationDecision: ${aggregateRecommendation.decision}.`)
   }
 }
